c901f79d8b25b7fb19a78860f27b2bc8e8145345
yashjain1974 <yash191174@gmail.com>
2025-08-01 21:57:20 +0530

    updated code

--- TransactionService/src/main/java/com/transaction/DemoApplication.java
+++ TransactionService/src/main/java/com/transaction/DemoApplication.java
@@ -3,11 +3,13 @@ package com.transaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients; // Correct annotation to enable Feign clients
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.transaction.proxyService") // IMPORTANT: Specifies the package where your Feign client interfaces are
 @EntityScan("com.transaction.model") // Scans for JPA entities
+@EnableDiscoveryClient // Enables service registration and discovery with Eureka
 public class DemoApplication {
 
 	public static void main(String[] args) {
